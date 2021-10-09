0c48793da2de360dc697653b55658f4fca1021b9
stanlds <42073065+stanlds@users.noreply.github.com>
2021-10-09 11:44:51 -0500

    Delete codebrakerAPI-master directory

--- codebrakerAPI-master/index.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-var app = require('./app');
-const PORT = process.env.PORT || 5000;
-
-//Crear servidor y ponerme a escuchar peticiones http
-app.listen(port, ()=>{
-        console.log('Servidor corriendo en http://localhost:'+PORT);
-})
