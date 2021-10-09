70a574680dd9b6f71f34b384cc4f49ccee713ee0
stanlds <42073065+stanlds@users.noreply.github.com>
2021-10-09 11:21:50 -0500

    Add files via upload

--- codebrakerAPI-master/index.js
+++ codebrakerAPI-master/index.js
@@ -1,9 +1,9 @@
 'use strict'
 
 var app = require('./app');
-var port = 4200;
+const PORT = process.env.PORT || 5000;
 
 //Crear servidor y ponerme a escuchar peticiones http
 app.listen(port, ()=>{
-        console.log('Servidor corriendo en http://localhost:'+port);
+        console.log('Servidor corriendo en http://localhost:'+PORT);
 })
